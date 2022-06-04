2b87a29cc16ba360ecea05a18d1d60a417e02bb0
oluwabukola tina <oluwabukolatina@gmail.com>
2022-06-04 19:46:11 +0100

    refactor

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
- function checkPhoneNumber(phoneNumber) {
-     function replaceValue(str, neww, old) {
-         return str.replace(neww, old);
-     }
-     if (phoneNumber.startsWith("2340")) {
-         console.log("here");
-         return `${replaceValue(phoneNumber, "2340", "+234")}`;
-     }
-     if (phoneNumber.startsWith("+0")) {
-         console.log("here 0");
+// exports.formatPhoneNumberToContainCountryCode = function() {
+module.exports = function formatPhoneNumberToContainCountryCode(string) {
+    console.log("This is a message from the demo package");
 
-         return `${replaceValue(phoneNumber, "+0", "+234")}`;
-     }
-     if (phoneNumber.startsWith("234")) {
-         console.log(phoneNumber, "numddddb");
-         return `${replaceValue(phoneNumber, "234", "+234")}`;
-     }
-     if (phoneNumber.startsWith("+234")) {
-         console.log(phoneNumber, "ndumb");
+    function replaceValue(str, newValue, oldValue) {
+        return str.replace(newValue, oldValue);
+    }
+    if (phoneNumber.startsWith("2340")) {
+        console.log("here");
+        return `${replaceValue(phoneNumber, "2340", "+234")}`;
+    }
+    if (phoneNumber.startsWith("+0")) {
+        console.log("here 0");
 
-         return `${replaceValue(phoneNumber, "+234", "+234")}`;
-     }
-     if (phoneNumber.startsWith("0")) {
-         console.log(phoneNumber, "n000umb");
+        return `${replaceValue(phoneNumber, "+0", "+234")}`;
+    }
+    if (phoneNumber.startsWith("234")) {
+        console.log(phoneNumber, "numddddb");
+        return `${replaceValue(phoneNumber, "234", "+234")}`;
+    }
+    if (phoneNumber.startsWith("+234")) {
+        console.log(phoneNumber, "ndumb");
 
-         return `${replaceValue(phoneNumber, "0", "+234")}`;
-     }
-     return `+234${phoneNumber}`;
- }
+        return `${replaceValue(phoneNumber, "+234", "+234")}`;
+    }
+    if (phoneNumber.startsWith("0")) {
+        console.log(phoneNumber, "n000umb");
+
+        return `${replaceValue(phoneNumber, "0", "+234")}`;
+    }
+    return `+234${phoneNumber}`;
+}
+
+function checkPhoneNumber(phoneNumber) {
+
+}

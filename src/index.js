1f1071da9ab90351f2c80b48e3d32a4254c7f510
oluwabukola tina <oluwabukolatina@gmail.com>
2022-06-05 00:39:34 +0100

    rm typescript

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function formatPhoneNumberToContainCountryCode(phoneNumber) {
+    function replaceValue(str, newValue, oldValue) {
+        return str.replace(newValue, oldValue);
+    }
+    if (phoneNumber.startsWith('+2340')) {
+        return `${replaceValue(phoneNumber, '+2340', '+234')}`;
+    }
+    if (phoneNumber.startsWith('2340')) {
+        return `${replaceValue(phoneNumber, '2340', '+234')}`;
+    }
+    if (phoneNumber.startsWith('+0')) {
+        return `${replaceValue(phoneNumber, '+0', '+234')}`;
+    }
+    if (phoneNumber.startsWith('234')) {
+        return `${replaceValue(phoneNumber, '234', '+234')}`;
+    }
+
+    if (phoneNumber.startsWith('0')) {
+        return `${replaceValue(phoneNumber, '0', '+234')}`;
+    }
+    return `+234${phoneNumber}`;
+}
+
+module.exports = formatPhoneNumberToContainCountryCode

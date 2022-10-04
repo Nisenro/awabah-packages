cdca5d1d321ba8a14a39f539fcc270ad65d23551
nedyudombat <nedyudombat@gmail.com>
2022-10-04 12:14:19 +0100

    Clean up processing charges package

--- processing-charges.js
+++ processing-charges.js
@@ -1,63 +1,78 @@
 import Dinero from "dinero.js";
 
 const localProcessingLowerfee = Dinero({
-    amount: 20,
-    currency: "NGN",
-    precision: 4,
+  amount: 20,
+  currency: "NGN",
+  precision: 4,
 });
 const localProcessingHigherfee = Dinero({
-    amount: 100,
-    currency: "NGN",
-    precision: 4,
+  amount: 100,
+  currency: "NGN",
+  precision: 4,
 });
 const paystackPercentageCharge = 1.5;
 const paystackProcessingLimit = Dinero({
-    amount: 2000,
-    currency: "NGN",
-    precision: 4,
+  amount: 2000,
+  currency: "NGN",
+  precision: 4,
 });
 const limit = Dinero({ amount: 4000, currency: "NGN", precision: 4 });
-let totalFee = Dinero({ amount: 0, currency: "NGN", precision: 4 });
-let totalProcessingFee = Dinero({ amount: 0, currency: "NGN", precision: 4 });
-let mininumProceesfinFee = Dinero({ amount: 100, currency: "NGN", precision: 4 });
 
-module.exports = function(amount) {
-    const formattedAmount = Dinero({
-        amount: Number(amount),
-        currency: "NGN",
-        precision: 4,
-    });
-    const paystackProcessingFee = formattedAmount.percentage(
-        paystackPercentageCharge,
-    );
+module.exports = function (amount) {
+  const formattedAmount = Dinero({
+    amount: Number(amount),
+    currency: "NGN",
+    precision: 4,
+  });
+  let totalFee = Dinero({ amount: 0, currency: "NGN", precision: 4 });
+  let awabahServiceCharge = Dinero({
+    amount: 0,
+    currency: "NGN",
+    precision: 4,
+  });
+  let totalProcessingFee = Dinero({ amount: 0, currency: "NGN", precision: 4 });
+  let mininumProceesingFee = Dinero({
+    amount: 100,
+    currency: "NGN",
+    precision: 4,
+  });
 
-    if (formattedAmount.lessThanOrEqual(limit)) {
-        totalProcessingFee = localProcessingLowerfee.add(paystackProcessingFee);
-    } else if (
-        formattedAmount.greaterThan(limit) &&
-        paystackProcessingFee.lessThan(paystackProcessingLimit)
-    ) {
-        totalProcessingFee = localProcessingHigherfee.add(paystackProcessingFee);
-    } else if (
-        formattedAmount.greaterThan(limit) &&
-        paystackProcessingFee.greaterThan(paystackProcessingLimit)
-    ) {
-        totalProcessingFee = localProcessingHigherfee.add(paystackProcessingLimit);
-    }
+  const paystackProcessingFee = formattedAmount.percentage(
+    paystackPercentageCharge,
+    "HALF_EVEN"
+  );
 
-    totalFee = formattedAmount.add(totalProcessingFee);
-    console.log("");
-    console.log("formattedAmount", formattedAmount);
-    console.log("totalProcessingFee", totalProcessingFee.getAmount());
-    console.log("paystackProcessingFee", paystackProcessingFee);
-    console.log("totalFee", totalFee);
-    console.log("");
+  if (formattedAmount.lessThanOrEqual(mininumProceesingFee)) {
     return {
-        totalFee,
-        paystackProcessingFee,
-        totalProcessingFee: totalProcessingFee
-            .multiply(10000)
-            .toFormat("$0,000.00"),
+      totalFee,
+      awabahServiceCharge,
+      totalProcessingFee,
+      paystackProcessingFee,
     };
+  }
 
-}
+  if (formattedAmount.lessThanOrEqual(limit)) {
+    totalProcessingFee = localProcessingLowerfee.add(paystackProcessingFee);
+    awabahServiceCharge = localProcessingLowerfee;
+  } else if (
+    formattedAmount.greaterThan(limit) &&
+    paystackProcessingFee.lessThan(paystackProcessingLimit)
+  ) {
+    totalProcessingFee = localProcessingHigherfee.add(paystackProcessingFee);
+    awabahServiceCharge = localProcessingHigherfee;
+  } else if (
+    formattedAmount.greaterThan(limit) &&
+    paystackProcessingFee.greaterThan(paystackProcessingLimit)
+  ) {
+    totalProcessingFee = localProcessingHigherfee.add(paystackProcessingLimit);
+    awabahServiceCharge = localProcessingHigherfee;
+  }
+
+  totalFee = formattedAmount.add(totalProcessingFee);
+  return {
+    totalFee,
+    awabahServiceCharge,
+    totalProcessingFee,
+    paystackProcessingFee,
+  };
+};
